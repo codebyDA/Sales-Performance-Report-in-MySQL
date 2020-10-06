08047df23de8a2532da5e8a17f7af4f61ee40636
Daniel Albesta <72337233+codebyDA@users.noreply.github.com>
2020-10-06 12:48:33 +0700

    customer analytics

--- SQL Queries/Customer Analytics_Queries.sql
+++ SQL Queries/Customer Analytics_Queries.sql
@@ -1,11 +1,25 @@
-SELECT TOP (10) [order_id]
-      ,[order_status]
-      ,[customer]
-      ,[order_date]
-      ,[order_quantity]
-      ,[sales]
-      ,[discount]
-      ,[discount_value]
-      ,[product_category]
-      ,[product_sub_category]
-  FROM [Sales Performance Report].[dbo].[dqlab_sales_store]
+/*
+      Promotion Effectiveness and Efficiency by Years
+      Calculate the burn rate of the promotion performed by overalls based on the year
+*/
+
+SELECT
+	year(order_date) as years,
+	count(distinct customer) as number_of_customer
+FROM
+	dqlab_sales_store
+WHERE
+	order_status = 'Order Finished'
+GROUP BY
+	year (order_date)
+
+
+console (output) ::
++-------+--------------------+
+| years | number_of_customer |
++-------+--------------------+
+|  2009 |                585 |
+|  2010 |                593 |
+|  2011 |                581 |
+|  2012 |                594 |
++-------+--------------------+
